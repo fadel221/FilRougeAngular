90447bbab259e01039ac073b3c65ded1a644cd27
fadel221 <gueyefadel22@gmail.com>
2020-12-07 15:53:32 +0000

    first commit angular

---
+++ src/app/connexion/connexion.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-connexion',
+  templateUrl: './connexion.component.html',
+  styleUrls: ['./connexion.component.css']
+})
+export class ConnexionComponent implements OnInit {
+public login="user";
+public pwd="12345";
+  
+
+  ngOnInit(): void {
+    
+  }
+
+}
